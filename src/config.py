6add6ee53b80703dd96b97a5f4b3d1f5b8bb9204
ogtay-latifli <ogtay.latifli.admission@gmail.com>
2026-07-12 17:33:28 +0000

    fixall

--- src/config.py
+++ src/config.py
@@ -7,5 +7,5 @@ next_year = "2026"
 
 # On GitHub, the URLs can omit the .html suffix and directories forward to
 # index.html inside. URLs are simplifies when enabled.
-github = True
+github = False
 
